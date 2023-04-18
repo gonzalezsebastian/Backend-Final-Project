74dfafbca136c57941b87310b48ac391fdc30bb6
Sebastian Enrique Gonzalez Benítez <71895275+gonzalezsebastian@users.noreply.github.com>
2023-04-18 18:19:36 -0500

    Fix: All User field are required for register

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   first_name: { type: String, required: true },
-  second_name: { type: String },
+  second_name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phone: { type: String, required: true },
